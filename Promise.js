200d46d0b931a616698b619cab245ee41358129f
啾呆 <2505434725@qq.com>
2023-01-18 08:45:27 +0800

    1.17手搓then

--- Promise.js
+++ Promise.js
@@ -3,15 +3,22 @@ function Promise(executor) {
     this.PromiseState='pending'
     this.PromiseResult=null
     const self = this
+    this.callbacks = []
     function resolve(data) {
         if(self.PromiseState !== 'pending') return;
         self.PromiseState='resolved'
         self.PromiseResult=data
+        self.callbacks.forEach(item => {
+            item.onResolved(data)
+        })
     }
     const reject = (data)=>{
         if(this.PromiseState !== 'pending') return;
         this.PromiseState='rejected'
         this.PromiseResult=data
+        this.callbacks.forEach(item =>{
+            item.onRejected(data)
+        })
     }
     try {
         executor(resolve,reject)
@@ -22,5 +29,16 @@ function Promise(executor) {
 }
 //添加一个then方法
 Promise.prototype.then=function (onResolved,onRejected){
-
+    if (this.PromiseState === 'resolved'){
+        onResolved(this.PromiseResult)
+    }
+    if (this.PromiseState === 'rejected') {
+        onRejected(this.PromiseResult)
+    }
+    if (this.PromiseState === 'pending') {
+        this.callbacks.push({
+            onResolved:onResolved,
+            onRejected:onRejected
+        })
+    }
 }
